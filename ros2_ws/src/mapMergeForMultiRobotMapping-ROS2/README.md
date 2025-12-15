2f33ec6c8f360f61ccf407d9eaad1f618d33dbfa
bing <b1417853077@outlook.com>
2025-12-15 14:40:14 +0800

    Force add map merge source code

---
+++ ros2_ws/src/mapMergeForMultiRobotMapping-ROS2/README.md
@@ -0,0 +1,36 @@
+# mapMergeForMultiRobotMapping-ROS2
+This ROS2 node subscribes to two different map messages, merges them, and 
+publishes the merged map to the /merge_map topic.
+
+## How does it work
+
+The Merge Map ROS2 package is designed to merge map messages from `/map1` and `/map2` topics, and publish the merged map to the `/merge_map` topic. To use this package, follow the instructions below:
+
+1. Copy the `merge_map` folder to the `src` directory of your ROS2 workspace.
+2. Run the following command: `ros2 launch merge_map merge_map_launch.py` This command starts the map merging node and opens an RViz2 window.
+3. In the RViz2 window, you can observe the merged map. Whenever one of the maps being merged is updated, the `merge_map` node will also update the merged map accordingly.
+
+Make sure to have ROS2 installed and properly configured in your environment before using this package. For more information, please refer to the ROS2 documentation.
+
+
+## launch.py customization
+
+The Merge Map ROS2 package can be customized and launched in your own `launch.py` file by adding the following code block. You can update `/yourMapName1` and `/yourMapName2` topics according to your project:
+
+```
+Node(
+    package='merge_map',
+    executable='merge_map',
+    output='screen',
+    parameters=[{'use_sim_time': True}],
+    remappings=[
+        ("/map1", "/yourMapName1"),
+        ("/map2", "/yourMapName2"),
+    ],
+),
+```
+
+## Example
+
+<img src="https://user-images.githubusercontent.com/87595266/231796297-5d719479-8866-40ee-b349-de3f9c70a976.png" width="950">
+
---
+++ ros2_ws/src/mapMergeForMultiRobotMapping-ROS2/merge_map/merge_map/merge_map.py
@@ -0,0 +1,106 @@
+# Authors: Abdulkadir Ture
+# Github : abdulkadrtr
+
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import OccupancyGrid
+import numpy as np
+
+def merge_maps(map1, map2, map3):
+    merged_map = OccupancyGrid()
+    merged_map.header = map1.header
+    merged_map.header.frame_id = 'merge_map'
+    
+    # 1. 计算三个地图的最小边界和最大边界
+    min_x = min(map1.info.origin.position.x, map2.info.origin.position.x, map3.info.origin.position.x)
+    min_y = min(map1.info.origin.position.y, map2.info.origin.position.y, map3.info.origin.position.y)
+    max_x = max(map1.info.origin.position.x + (map1.info.width * map1.info.resolution),
+                map2.info.origin.position.x + (map2.info.width * map2.info.resolution),
+                map3.info.origin.position.x + (map3.info.width * map3.info.resolution))
+    max_y = max(map1.info.origin.position.y + (map1.info.height * map1.info.resolution),
+                map2.info.origin.position.y + (map2.info.height * map2.info.resolution),
+                map3.info.origin.position.y + (map3.info.height * map3.info.resolution))
+    
+    merged_map.info.origin.position.x = min_x
+    merged_map.info.origin.position.y = min_y
+    merged_map.info.resolution = min(map1.info.resolution, map2.info.resolution, map3.info.resolution)
+    merged_map.info.width = int(np.ceil((max_x - min_x) / merged_map.info.resolution))
+    merged_map.info.height = int(np.ceil((max_y - min_y) / merged_map.info.resolution))
+    merged_map.data = [-1] * (merged_map.info.width * merged_map.info.height)
+
+    # 2. 处理 Map 1 (优先级最高，直接覆盖)
+    for y in range(map1.info.height):
+        for x in range(map1.info.width):
+            i = x + y * map1.info.width
+            merged_x = int(np.floor((map1.info.origin.position.x + x * map1.info.resolution - min_x) / merged_map.info.resolution))
+            merged_y = int(np.floor((map1.info.origin.position.y + y * map1.info.resolution - min_y) / merged_map.info.resolution))
+            merged_i = merged_x + merged_y * merged_map.info.width
+            if 0 <= merged_i < len(merged_map.data): # 增加越界保护
+                merged_map.data[merged_i] = map1.data[i]
+
+    # 3. 处理 Map 2 (只填充未知区域 -1)
+    for y in range(map2.info.height):
+        for x in range(map2.info.width):
+            i = x + y * map2.info.width
+            merged_x = int(np.floor((map2.info.origin.position.x + x * map2.info.resolution - min_x) / merged_map.info.resolution))
+            merged_y = int(np.floor((map2.info.origin.position.y + y * map2.info.resolution - min_y) / merged_map.info.resolution))
+            merged_i = merged_x + merged_y * merged_map.info.width
+            if 0 <= merged_i < len(merged_map.data):
+                if merged_map.data[merged_i] == -1:
+                    merged_map.data[merged_i] = map2.data[i]
+
+    # 4. 处理 Map 3 (只填充未知区域 -1)
+    for y in range(map3.info.height):
+        for x in range(map3.info.width):
+            i = x + y * map3.info.width
+            merged_x = int(np.floor((map3.info.origin.position.x + x * map3.info.resolution - min_x) / merged_map.info.resolution))
+            merged_y = int(np.floor((map3.info.origin.position.y + y * map3.info.resolution - min_y) / merged_map.info.resolution))
+            merged_i = merged_x + merged_y * merged_map.info.width
+            if 0 <= merged_i < len(merged_map.data):
+                if merged_map.data[merged_i] == -1:
+                    merged_map.data[merged_i] = map3.data[i]
+    
+    return merged_map
+
+class MergeMapNode(Node):
+    def __init__(self):
+        super().__init__('merge_map_node')
+        self.publisher = self.create_publisher(OccupancyGrid, '/merge_map', 10)
+        
+        # 修复：使用不同的变量名存储 subscription，否则会被覆盖
+        self.sub1 = self.create_subscription(OccupancyGrid, '/map1', self.map1_callback, 10)
+        self.sub2 = self.create_subscription(OccupancyGrid, '/map2', self.map2_callback, 10)
+        self.sub3 = self.create_subscription(OccupancyGrid, '/map3', self.map3_callback, 10)
+        
+        self.map1 = None
+        self.map2 = None
+        self.map3 = None
+
+    def map1_callback(self, msg):
+        self.map1 = msg
+        if self.map2 is not None and self.map3 is not None:
+            msg = merge_maps(self.map1, self.map2, self.map3)
+            self.publisher.publish(msg)
+    
+    def map2_callback(self, msg):
+        self.map2 = msg
+        if self.map1 is not None and self.map3 is not None:
+            msg = merge_maps(self.map1, self.map2, self.map3)
+            self.publisher.publish(msg)
+
+    # 修复：添加缺少的 map3 回调函数
+    def map3_callback(self, msg):
+        self.map3 = msg
+        if self.map1 is not None and self.map2 is not None:
+            msg = merge_maps(self.map1, self.map2, self.map3)
+            self.publisher.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    merge_map_node = MergeMapNode()
+    rclpy.spin(merge_map_node)
+    merge_map_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
